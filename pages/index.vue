f995f3928608cf38948a7b0f450d50e3d9eb5bb8
Aranha Agency <=>
2019-10-27 13:05:55 -0300

    start project

---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        codingsh | @codingsh
+      </h1>
+      <h2 class="subtitle">
+        developer notes
+      </h2>
+      <div class="links">
+        <a
+          href="https://twitter.com/codingsh"
+          target="_blank"
+          class="button--green">Twitter</a>
+        <a
+          href="https://github.com/developerfred"
+          target="_blank"
+          class="button--grey">GitHub</a>
+        <a
+          href="https://instagram.com/codingshxyz"
+          target="_blank"
+          class="button--grey">Intagram</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
