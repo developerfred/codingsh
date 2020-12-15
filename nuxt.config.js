36a8a672e1a82a72a0c1f89542d591d09e3ca7f2
codingsh <codingsh@pm.me>
2020-12-15 14:10:37 +0000

    deploy ifps

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,18 +24,18 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+    // extend (config, { isDev, isClient }) {
+    //   if (isDev && isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
   }
-  
+
 }
 
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
